c2d0ad9ddc2047c178f489d4956bd75657ec3068
muaXiaoNing <43727142+muaXiaoNing@users.noreply.github.com>
2019-12-02 21:01:26 +0800

    day01
    1_4 writing only using one dimension matrix,maybe with bugs.
    And ,the next day is 2400 running day,f**k.

---
+++ prog1_1.m
@@ -0,0 +1,13 @@
+value=[-3;1;1];
+	%价值向量
+source=[1,-2,1;4,-1,-2];%资源矩阵
+resource=[11;-3];
+%资源向量
+aeq=[-2,0,1];%等式区域
+beq=1;
+
+[x,y]=linprog(value,source,resource,aeq,beq,zeros(3,1));%x为最优化矩阵，y为最优化结果
+x
+
+y=-y
+%反式输出结果
