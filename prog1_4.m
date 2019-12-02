603dc4b1d7c1fa1c36df7925c15f3fc6e7b873d9
muaXiaoNing <43727142+muaXiaoNing@users.noreply.github.com>
2019-12-02 21:05:13 +0800

    Update prog1_4.m

--- prog1_4.m
+++ prog1_4.m
@@ -1,16 +1,9 @@
 f=[31;38;35;28.5];
-
 A=[18,15,23,12;48,65,58,39];
-
 b=[34;1848.75];
-
 [x,y]=linprog(-f,A,b,[],[],zeros(4,1));
-
 fprintf('optimazed matrix is:');
-
 x
-
 fprintf('maximum load is:');
 y=-y
-
-%好像做错了？以后修改
+%艧膫膸艅脳枚麓铆脕脣艁偶艊脭艧贸膼泞赂脛
